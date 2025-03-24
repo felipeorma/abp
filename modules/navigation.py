4a99d8296efc31a03b7e943ab6c999737f67f45a
Felipe Ormazabal <97199418+felipeorma@users.noreply.github.com>
2025-03-24 14:50:58 -0600

    Create navigation.py

---
+++ modules/navigation.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def show_navigation():
+    st.sidebar.title("Navegación")
+    return st.sidebar.radio(
+        "Seleccionar módulo:",
+        ("🏟️ Registro en Vivo", "📊 Panel Analítico"),
+        index=0
+    )
