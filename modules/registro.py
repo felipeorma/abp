2adcef3da8a8e4b6a1d8c89e8c9df8e0cb959d82
Felipe Ormazabal <97199418+felipeorma@users.noreply.github.com>
2025-03-25 09:39:29 -0600

    Update registro.py

--- modules/registro.py
+++ modules/registro.py
@@ -146,10 +146,11 @@ def mostrar_formulario(jugadores, equipos, zonas):
         datos["Estrategia"] = col2.selectbox("Estrategia", ["Sí", "No"])
         datos["Tipo Ejecución"] = col2.selectbox("Tipo de ejecución", ["Centro", "Pase corto", "Disparo directo"])
 
-        if st.button("✅ Registrar Acción"):
-            return datos
-        else:
-            return None
+    # ✅ Botón de registrar fuera del container
+    if st.button("✅ Registrar Acción"):
+        return datos
+    else:
+        return None
 
 
 
