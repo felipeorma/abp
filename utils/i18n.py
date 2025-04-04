dc2c6ae06526415bc946c320366ac534419d096c
Felipe Ormazabal <97199418+felipeorma@users.noreply.github.com>
2025-04-04 15:10:46 -0600

    Update i18n.py

--- utils/i18n.py
+++ utils/i18n.py
@@ -1,7 +1,12 @@
 import json
+import os
 from typing import Dict
 
 def get_text(lang: str, key: str) -> str:
-    with open("utils/i18n.json", "r", encoding="utf-8") as f:
+    # Obtener la ruta absoluta del archivo
+    current_dir = os.path.dirname(__file__)
+    file_path = os.path.join(current_dir, "i18n.json")
+    
+    with open(file_path, "r", encoding="utf-8") as f:
         translations = json.load(f)
     return translations.get(lang, {}).get(key, f"[{key}]")
