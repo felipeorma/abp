4ca91ffab424792053eaba16f27c1da6afb01e7c
Felipe Ormazabal <97199418+felipeorma@users.noreply.github.com>
2025-04-06 21:00:20 -0600

    Update analitica.py

--- modules/analitica.py
+++ modules/analitica.py
@@ -360,12 +360,12 @@ def configurar_descarga(lang: str, df):
     )
 
 # --- Footer signature ---
-st.markdown(
-    """
-    <hr style='margin-top: 40px; margin-bottom: 10px'>
-    <div style='text-align: center; font-size: 14px; color: gray;'>
-        <strong>Felipe Ormazabal</strong><br>Soccer Scout - Data Analyst
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+    st.markdown(
+        """
+        <hr style='margin-top: 40px; margin-bottom: 10px'>
+        <div style='text-align: center; font-size: 14px; color: gray;'>
+            <strong>Felipe Ormazabal</strong><br>Soccer Scout - Data Analyst
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
